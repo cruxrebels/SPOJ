c3312488a9ca93d89effd3900bc47741fd90de02
Abhishek Agrawal <cruxrebels@gmail.com>
2016-09-29 21:57:46 +0530

    Add Prime1.cpp

--- Prime Generator - PRIME1/Prime Generator - PRIME1/Prime Generator - PRIME1/Prime1.cpp
+++ Prime Generator - PRIME1/Prime Generator - PRIME1/Prime Generator - PRIME1/Prime1.cpp
@@ -9,11 +9,20 @@ void PrimeGenerator(size_t lowerLimit, size_t higherLimit)
 {
 	std::vector<bool> prime((higherLimit - lowerLimit) + 1, true);
 
-	for (auto i = lowerLimit; i < higherLimit; ++i)
+	for (auto i = lowerLimit; i*i <= higherLimit; ++i)
 	{
-		for (auto j = i; j <=)
+		if (prime[i] == true)
+		{
+			for (auto j = 2*i; j <= higherLimit; j += i)
+				prime[j] = false;
+		}		
 	}
 
+	for (const auto& p : prime)
+	{
+		if (prime[p])
+			std::cout << p << std::endl;
+	}
 }
 
 int main()
